92105f2c72520fc1e5b0171a8127716c0f07251f
AbnerTeng <abnerteng16@gmail.com>
2022-07-07 16:19:23 +0800

    cpp linkedlist

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class LinkedList;    // 為了將class LinkedList設成class ListNode的friend,
+                     // 需要先宣告
+class ListNode{
+private:
+    int data;
+    ListNode *next;
+public:
+    ListNode():data(0),next(0){};
+    ListNode(int a):data(a),next(0){};
+
+    friend class LinkedList;
+};
+
+class LinkedList{
+private:
+    // int size;                // size是用來記錄Linked list的長度, 非必要
+    ListNode *first;            // list的第一個node
+public:
+    LinkedList():first(0){};
+    void PrintList();           // 印出list的所有資料
+    void Push_front(int x);     // 在list的開頭新增node
+    void Push_back(int x);      // 在list的尾巴新增node
+    void Delete(int x);         // 刪除list中的 int x
+    void Clear();               // 把整串list刪除
+    void Reverse();             // 將list反轉: 7->3->14 => 14->3->7
+};
+
+void LinkedList::PrintList(){
+    if(first == 0){
+        cout << "List is empty .\n";
+        return;
+    }
+
+    ListNode *current = first;
+    while(current != 0){
+        cout << current -> data << " ";
+        current = current -> next;
+    }
+    cout << endl;
+}
+
+void LinkedList::Push_front(int x){
+    ListNode *newNode = new ListNode(x);
+    newNode -> next = first;
+    first = newNode; 
+}
